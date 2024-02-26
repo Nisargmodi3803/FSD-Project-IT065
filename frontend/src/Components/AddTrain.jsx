64dd2c82b62c4523d233529a62d38a54db68ba7a
Nisarg Modi <124357940+Nisargmodi3803@users.noreply.github.com>
2024-02-26 23:35:57 +0530

    Add Train

--- frontend/src/Components/AddTrain.jsx
+++ frontend/src/Components/AddTrain.jsx
@@ -8,49 +8,51 @@ import "./AddTrain.css"
 
 export default function AddTrain() {
   return (
-    <div className='wrapper'>
-  <form>
-    <h1>ADD TRAIN</h1>
-    <br/>
-    <div className='input-pair'>
-      <div className='input-box'>
-        <FaTrain />
-        <input type='text' placeholder='Train Name' />
+    <section>
+      <div className='wrapper'>
+        <form className='background'>
+          <h1>ADD TRAIN</h1>
+          <br />
+          <div className='input-pair'>
+            <div className='input-box'>
+              <FaTrain className='icon' />
+              <input type='text' placeholder='Train Name' />
+            </div>
+            <div className='input-box'>
+              <AiOutlineFieldNumber className='icon' />
+              <input type='text' placeholder='Train Number' />
+            </div>
+          </div>
+          <br />
+          <br />
+          <div className='input-pair'>
+            <div className='input-box'>
+              <FaLocationDot className='icon' />
+              <input type='text' placeholder='From Location' />
+            </div>
+            <div className='input-box'>
+              <FaLocationDot className='icon' />
+              <input type='text' placeholder='To Location' />
+            </div>
+          </div>
+          <br />
+          <br />
+          <div className='input-pair'>
+            <div className='input-box'>
+              <FaRupeeSign className='icon' />
+              <input type='number' placeholder='Ticket Price' />
+            </div>
+            <div className='input-box'>
+              <MdOutlineAirlineSeatReclineExtra className='icon' />
+              <input type='number' placeholder='Number of Seats' />
+            </div>
+          </div>
+          <br />
+          <div className='button'>
+            <button type='submit'>ADD</button>
+          </div>
+        </form>
       </div>
-      <div className='input-box'>
-        <AiOutlineFieldNumber/>
-        <input type='text' placeholder='Train Number' />
-      </div>
-    </div>
-    <br/>
-    <br/>
-    <div className='input-pair'>
-      <div className='input-box'>
-        <FaLocationDot/>
-        <input type='text' placeholder='From Location' />
-      </div>
-      <div className='input-box'>
-        <FaLocationDot/>
-        <input type='text' placeholder='To Location' />
-      </div>
-    </div>
-    <br/>
-    <br/>
-    <div className='input-pair'>
-      <div className='input-box'>
-        <FaRupeeSign/>
-        <input type='number' placeholder='Ticket Price' />
-      </div>
-      <div className='input-box'>
-        <MdOutlineAirlineSeatReclineExtra/>
-        <input type='number' placeholder='Number of Seats' />
-      </div>
-    </div>
-    <br/>
-    <div className='button'>
-      <button type='submit'>ADD</button>
-    </div>
-  </form>
-</div>
+    </section>
   )
 }
--- frontend/src/Components/TrainList.jsx
+++ frontend/src/Components/TrainList.jsx
@@ -1,7 +1,38 @@
 import React from 'react'
+import { FaLocationDot } from "react-icons/fa6";
+import './TrainList.css'
 
 export default function TrainList() {
   return (
-    <div>TrainList</div>
+    <div className='wrapper'>
+      <form>
+        <h1>TRAIN LIST</h1>
+        <br />
+        <div className='input-pair'>
+          <div className='input-box'>
+            <FaLocationDot className='icon' />
+            <input type='text' placeholder='From Location' />
+          </div>
+          <div className='input-box'>
+            <FaLocationDot className='icon' />
+            <input type='text' placeholder='To Location' />
+          </div>
+          <div className='button'>
+            <button type='submit'>Search</button>
+          </div>
+          <div className='button'>
+            <button type='submit'>Reset</button>
+          </div>
+        </div>
+      </form>
+      {/* <table>
+        <tr>Train Name</tr>
+        <tr>Train Number</tr>
+        <tr>From</tr>
+        <tr>To</tr>
+        <tr>Price</tr>
+        <tr>Available Seats</tr>
+      </table> */}
+    </div>
   )
 }
