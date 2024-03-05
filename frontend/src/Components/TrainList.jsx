6ddfa92e7dcf8c3fd4b969ae57582e7fc291e7e7
Nisarg Modi <124357940+Nisargmodi3803@users.noreply.github.com>
2024-03-06 00:07:07 +0530

    UpdateTrain Dialog box css

--- frontend/src/Components/TrainList.jsx
+++ frontend/src/Components/TrainList.jsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaLocationDot } from "react-icons/fa6";
 import './TrainList.css';
 import { useNavigate } from 'react-router-dom';
+import { Button, Dialog, DialogTitle, DialogContent, DialogActions, TextField } from '@mui/material';
 
 export default function TrainList() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  const [selectedCourse, setSelectedCourse] = useState(null);
+  const [isUpdateFormOpen, setIsUpdateFormOpen] = useState(false);
+  const [trainDetails, setTrainDetails] = useState({
+    name: 'Gujarat Queen',
+    number: '19033',
+    from: 'Valsad',
+    to: 'Ahmedabad',
+    price: '300',
+    availableSeats: '50'
+  });
+
+  const handleUpdate = () => {
+    setIsUpdateFormOpen(true);
+  };
+
+  const handleClose = () => {
+    setIsUpdateFormOpen(false);
+  };
+
+  const handleUpdateSubmit = () => {
+    setIsUpdateFormOpen(false);
+  };
+
   return (
     <div className='trainlist'>
       <div className='wrap'>
@@ -45,14 +69,14 @@ export default function TrainList() {
                 <th>Update Train</th>
               </tr>
               <tr>
-                <td>Gujarat Queen</td>
-                <td>19033</td>
-                <td>Valsad</td>
-                <td>Ahmedabad</td>
-                <td>300</td>
-                <td>50</td>
+                <td>{trainDetails.name}</td>
+                <td>{trainDetails.number}</td>
+                <td>{trainDetails.from}</td>
+                <td>{trainDetails.to}</td>
+                <td>{trainDetails.price}</td>
+                <td>{trainDetails.availableSeats}</td>
                 <td><button className='delete-button'>Delete</button></td>
-                <td><button className='update-button' onClick={() => navigate('/trainList/updateTrain')}>update</button></td>
+                <td><button className='update-button' onClick={() => handleUpdate()}>update</button></td>
               </tr>
             </thead>
             <tbody>
@@ -60,6 +84,42 @@ export default function TrainList() {
           </table>
         </div>
       </div>
+
+      <Dialog open={isUpdateFormOpen} onClose={handleClose} className='dialog-container'>
+        <DialogTitle className='dialog-title'>Update Train</DialogTitle>
+        <DialogContent className='dialog-content'>
+          <div className='dialog-text-field-container'>
+            <div className='dialog-text-label'>Train Name</div>
+            <TextField fullWidth value={trainDetails.name} className='dialog-text-field' onChange={(e) => setTrainDetails({ ...trainDetails, name: e.target.value })} />
+          </div>
+          <div className='dialog-text-field-container'>
+            <div className='dialog-text-label'>Train Number</div>
+            <TextField fullWidth value={trainDetails.number} className='dialog-text-field' onChange={(e) => setTrainDetails({ ...trainDetails, number: e.target.value })}/>
+          </div>
+          <div className='dialog-text-field-container'>
+            <div className='dialog-text-label'>Train From</div>
+            <TextField fullWidth value={trainDetails.from} className='dialog-text-field' onChange={(e) => setTrainDetails({ ...trainDetails, from: e.target.value })}/>
+          </div>
+          <div className='dialog-text-field-container'>
+            <div className='dialog-text-label'>Train To</div>
+            <TextField fullWidth value={trainDetails.to} className='dialog-text-field' onChange={(e) => setTrainDetails({ ...trainDetails, to: e.target.value })}/>
+          </div>
+          <div className='dialog-text-field-container'>
+            <div className='dialog-text-label'>Train Price</div>
+            <TextField fullWidth value={trainDetails.price} className='dialog-text-field' onChange={(e) => setTrainDetails({ ...trainDetails, price: e.target.value })}/>
+          </div>
+          <div className='dialog-text-field-container'>
+            <div className='dialog-text-label'>Train Available Seat</div>
+            <TextField fullWidth value={trainDetails.availableSeats} className='dialog-text-field' onChange={(e) => setTrainDetails({ ...trainDetails, availableSeats: e.target.value })}/>
+          </div>
+        </DialogContent>
+        <DialogActions className='dialog-actions'>
+          <Button onClick={handleClose} className='dialog-button'>Cancel</Button>
+          <Button onClick={handleUpdateSubmit} variant="contained" color="primary" className='dialog-button'>Update</Button>
+        </DialogActions>
+      </Dialog>
+
+
     </div>
   );
 }
